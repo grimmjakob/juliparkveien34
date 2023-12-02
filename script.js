35bd5b74b4d6db4da4b48041c2fcb8042ca5e242
grimmjakob <152562183+grimmjakob@users.noreply.github.com>
2023-12-02 13:03:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,48 @@ const boxes = document.querySelectorAll('.box');
 // Function to display the question and options
 function displayQuestionAndOptions(day) {
     // You can create an array of questions and options corresponding to each day
+    // Define hints for box 2
+const hintsForBox2 = [
+    "Hint 1: Det er en julefilm",
+    "Hint 2: Den inneholder et Murmeldyr",
+    "Hint 3: Legenden Bill er med i filmen",
+    "Hint 4: Handlingen er ganske så repiterende",
+    "Hint 5: En ny dag truer"
+];
+
+// Variable to keep track of the current hint index for box 2
+let currentHintIndexForBox2 = 0;
+
+// Function to display hints and allow guesses for box 2
+function displayHintsAndGuessForBox2() {
+    if (currentHintIndexForBox2 < hintsForBox2.length) {
+        // Display the current hint for box 2
+        alert(hintsForBox2[currentHintIndexForBox2]);
+
+        // Ask the user to make a guess for box 2
+        const userGuess = prompt("Hvilken film tror du det er?");
+        
+        // Check if the user's guess is correct for box 2
+        if (userGuess && userGuess.toLowerCase() === "Groundhog day") {
+            alert("Riktig!!");
+        } else {
+            // Increment the hint index for box 2 for the next hint
+            currentHintIndexForBox2++;
+            // Display the next hint and allow another guess for box 2
+            displayHintsAndGuessForBox2();
+        }
+    } else {
+        // No more hints left for box 2, display a message
+        alert("Bedre lykke i morgen");
+    }
+}
+
+// Add click event listener to box 2
+boxes[1].addEventListener('click', () => {
+    // Call the function to display hints and allow guesses for box 2
+    displayHintsAndGuessForBox2();
+});
+
     const questionsAndOptions = [
         {
             question: "Vi begynner det hele med en oscar-nominert film fra 2007. Dette er en film om å finne seg selv og man gjøre med livet på denne kloden. Dette er en film jeg personlig trenger å se, men er ikke blitt vist på hector enda pga så og si alle andre uten om meg har sett den. Så siden flere er vekke denne lørdagskvelden, griper jeg denne muligheten.",
@@ -11,9 +53,9 @@ function displayQuestionAndOptions(day) {
             correctAnswer: "Into the wild"
         },
         {
-            question: "What is the capital of France?",
-            options: ["Paris", "Berlin", "Madrid"],
-            correctAnswer: "Paris"
+            question: "trykk en gang til på boxen for å få et hint",
+            options: [""],
+            correctAnswer: "Groundhog day"
         },
         {
             question: "What is the capital of France?",
